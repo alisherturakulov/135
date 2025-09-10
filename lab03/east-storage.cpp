485afb1080ef8b50544d770f4c3463da45b14beb
alisherturakulov <aturakulov100@gmail.comls>
2025-09-10 13:46:35 -0400

    add check for case when date input is not in set

--- lab03/east-storage.cpp
+++ lab03/east-storage.cpp
@@ -6,10 +6,10 @@ Instructor: Tong Yi
 Assignment: Lab3A
 
 This program takes a data in MM/DD/YYYY format and uses the fstream library to read a tsv file with data on water and elevations 
-and print the East Basin sotrage levels on that date
+and print the East Basin storage levels on that date
 */
 
-#include <iostream>
+#include <iostream>//include more libraries due to use of file input and error checking
 #include <string>
 #include <cstdlib>
 #include <climits>
@@ -29,18 +29,24 @@ int main(){
     double EastSt, EastEl, WestSt, WestEl;
     std::cout << "Enter date: ";
     std::cin >> targetDate;
+    bool found = false;
+
     while(fin >> date >> EastSt >> EastEl >> WestSt >> WestEl){
-        //loop until we get the target row 
-        fin.ignore(INT_MAX, '\n');
+        //loop until we get the target row, close file connection when found
+        fin.ignore(INT_MAX, '\n'); 
         if(date == targetDate){
+            found = true;
             fin.close();
         }
         
     }
     
-    std::cout << "East basin storage: " << EastSt << " billion gallons" << std::endl;
+    //incase a date doesn't exist in the file
+    if(found){
+        std::cout << "East basin storage: " << EastSt << " billion gallons" << std::endl;
+    }
     
-    //fin.close();
+    //fin.close(); moved close into loop to terminate
 
     return 0;
 }
