76f3ff53029781ad0fa7e76d2abf792a2de2e567
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-10-08 08:03:25 -0400

    write Lab5B and C

--- labs/lab05/numbers.cpp
+++ labs/lab05/numbers.cpp
@@ -2,17 +2,41 @@
 Author: Alisherjon Turakulov
 Course: CS 135
 Instructor: Tong Yi
-Assingnment: Lab5A
+Assingnment: Lab5AtoG
 
 Takes two integers, n and d. Prints whether n is divisible by d.
 */
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 //short circuit if d is zero
 bool isDivisibleBy(int n, int d){
     return d != 0 && n % d ==0;
+}
+
+bool isPrime(int n){
+	double end = std::sqrt(n);
+	if(n <= 1){
+		return false;
+	}else if(n <= 3){
+		return true;
+	}
+	for(int i=2; i< end; ++i){
+		if(n % i == 0){
+			 return false;
+		}
+	}
+	return true;
+}
+
+int nextPrime(int n){
+	//start by checking n+1 and continue if not prime
+	while(!isPrime(++n)){
+		//keep looping until its prime
+	}
+	return n;
 }
 
 int main(){
@@ -22,14 +46,25 @@ int main(){
     std::cin >> n;
     std::cout << "Enter divisor: ";
     std::cin >> d;
-
-
-    //print result of division
+	//print result of division
     if(isDivisibleBy(n, d) ){
         std::cout << "Yes\n";
     }else{
         std::cout <<"No\n";
     }
-
+	
+	std::cout << "Enter a number to check if it's prime: ";
+	std::cin >> n;
+	if(isPrime(n)){
+		std::cout << "Yes\n";
+	}else{
+		std::cout << "No\n";
+	}
+	
+	std::cout << "Enter a number to find the next prime: ";
+	std::cin >> n;
+	std::cout << "Next prime after " << n << " is: " << nextPrime(n) << '\n';
+	
+	
     return 0;
 }
