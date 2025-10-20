81d7659c17e5a1fca43b984c1732e910c9de8234
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-10-19 22:45:05 -0400

    create tests for encryptVigenere and implementation

---
+++ labs/lab06/vigenere.cpp
@@ -0,0 +1,70 @@
+/*
+Author: Alisherjon Turakulov
+Course: CS 135
+Instructor: Tong Yi
+Assignment: Lab6C
+
+Implements a functions encryptVigenere that takes a plaintext string,
+keyword string, and uses the all lowercase keyword to shift the characters
+in the plaintext and returns the encrypted string.
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+//function forward declaration
+std::string encryptVigenere(std::string plaintext, std::string keyword);
+
+//helper function to apply caesar cipher shift
+char shiftChar(char& ch, int shift);
+
+int main(){
+	std::string plaintext{};
+	std::string keyword{};
+	std::cout << "Enter plaintext: ";
+	std::getline(std::cin, plaintext);
+	std::cout << "Enter keyword  : ";
+	std::cin >> keyword;
+	std::string cipherText = encryptVigenere(plaintext, keyword);
+	std::cout << "CipherText     : " << cipherText << '\n';
+	return 0;
+}
+
+//@params char to shift and amount
+//@return shifted char
+char shiftChar(char& ch, int shift){
+	//check 
+	if(!std::isalpha(ch)){
+		return ch;
+	}
+	int shiftChar = shift+ch;
+	if(ch < 91 && (shift + (int)ch > 90)){
+		
+	}
+	
+	return '0';
+}
+
+//@params plaintext string to apply vigenere 
+//        shift using given lowercase keyword 
+//@return the encrypted plaintext string
+std::string encryptVigenere(std::string plaintext, std::string keyword){
+	//for cycling through keyword when applying it to encrypt
+	int keyIndex{};
+	int keyLen = keyword.length();
+	//plaintext is pass by value, so we'll directly modify it
+	for(std::size_t i =0; i< plaintext.length(); ++i){
+		//only shift letters
+		char curr = plaintext[i];
+		if(std::isalpha(curr)){
+			
+			int shift = keyword[keyIndex]-'a';
+			char newChar = shiftChar(curr, shift);
+			plaintext[i] = newChar;
+			keyIndex = (keyIndex+1) % keyLen;
+		}
+	}
+	//return now encrypted plaintext
+	return plaintext;
+}
