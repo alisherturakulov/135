0345d6f8b2e11b61d110ef62ddd69a5acd5261ec
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-11-02 19:57:24 -0500

    fix double counting error with closing braces inline if statements

--- labs/lab07/indent.cpp
+++ labs/lab07/indent.cpp
@@ -15,14 +15,38 @@ a count of the curly braces.
 
 //function to remove leading ws
 std::string removeLeadingSpaces(std::string line);
-
 int countChar(std::string line, char c);
 
 int main(){
-	std::string line{};
+	std::string line;
+	int numBraces = 0;
 	while(getline(std::cin, line)){
 		std::string unindented = removeLeadingSpaces(line);
-		std::cout << unindented << '\n';
+		std::string indented = unindented;
+		int countOpen = countChar(indented, (char) 123);
+		int countClose = countChar(indented, (char) 125);
+		if(indented[0] == '}'){
+			--numBraces;
+		}
+		//subtract from braces if theres a closing brace
+		
+		//add 1 tab for each open brace
+		for(int i = 0; i<numBraces; ++i){
+			indented = '\t' + indented;
+		}
+		
+		if(countOpen >= countClose){
+			numBraces += countOpen - countClose;
+		}
+		//to prevent double counting closing braces
+		if(unindented[0] == '}' && countOpen == countClose){
+			++numBraces;
+		}
+		
+		//increment for future indents
+		
+		
+		std::cout << indented << '\n';
 	}
 	
 	return 0;
@@ -33,7 +57,7 @@ int main(){
 //@param line, a string with one line of code
 //@reutrn the unindented string
 std::string removeLeadingSpaces(std::string line){
-	std::string unindented{};
+	std::string unindented;
 	for(int i =0; i<line.length(); ++i){
 		if(!std::isspace(line[i])){
 			unindented = line.substr(i);
@@ -49,9 +73,9 @@ std::string removeLeadingSpaces(std::string line){
 //@param char c, char to count
 //@return number of occurences of char in line
 int countChar(std::string line, char c){
-	int count{};
-	for(char& ch: line){
-		if(ch == c){
+	int count=0;
+	for(int i=0; i<line.length(); ++i){
+		if(line[i] == c){
 			++count;
 		}
 	}
