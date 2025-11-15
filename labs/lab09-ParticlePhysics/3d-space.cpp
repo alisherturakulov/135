5bca5e595eeabd69e8bb718c75b68d92785e483a
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-11-14 19:59:37 -0500

    write declarations for Coord3D cast and length function

---
+++ labs/lab09-ParticlePhysics/3d-space.cpp
@@ -0,0 +1,36 @@
+/*
+Author: Alisherjon Turakulov
+Course: CS 135
+Instructor: Tong Yi
+Assignment: Lab 9A
+
+Given a pointer to a Coord3D that stores double coordinates x, y, z
+defines a function length, which returns the euclidean distance
+from origin
+*/
+
+#include <iostream>
+#include <string>
+
+//3 dimensional coordinate class definition
+class Coord3D{
+	public:
+		double x;
+		double y;
+		double z;
+};
+
+/** Returns the distance to the origin of a Coord3D
+  *@param p a pointer to a Coord3D object
+  *@return a double, the distance to the origin
+  */
+double length(Coord3D *p){
+	return 0.0;
+}
+
+
+int main(){
+	Coord3D coord{10, 10, 10};
+	std::cout << "x " << coord.x << std::endl;
+	return 0;
+}
