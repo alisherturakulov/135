6cda04d89969c8bdf8a3dd76037beaea3f39c2e1
Alisherjon Turakulov <aturakulov100@gmail.com>
2025-11-14 20:39:21 -0500

    write and test length function

--- labs/lab09-ParticlePhysics/3d-space.cpp
+++ labs/lab09-ParticlePhysics/3d-space.cpp
@@ -6,11 +6,12 @@ Assignment: Lab 9A
 
 Given a pointer to a Coord3D that stores double coordinates x, y, z
 defines a function length, which returns the euclidean distance
-from origin
+from origin 
 */
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 //3 dimensional coordinate class definition
 class Coord3D{
@@ -25,12 +26,18 @@ class Coord3D{
   *@return a double, the distance to the origin
   */
 double length(Coord3D *p){
-	return 0.0;
+	//using euclidean distance formula
+	double x = (*p).x; //dereferencing and using access by reference
+	double y = p->y; //access by pointer
+	double z = (*p).z;
+	double distxyz = std::sqrt(x*x + y*y + z*z);
+	return distxyz;
 }
 
 
 int main(){
 	Coord3D coord{10, 10, 10};
-	std::cout << "x " << coord.x << std::endl;
+	//std::cout << "x " << coord.x << std::endl;
+	std::cout << "should be  between 17 and 18: " << length(&coord) << std::endl; //pass address of coord
 	return 0;
 }
