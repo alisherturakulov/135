55d86597d2c1fa9b35aec6f72f1f5d7f7e98c715
alisherturakulov <aturakulov100@gmail.comls>
2025-11-24 00:07:40 -0500

    write 10D

--- labs/lab10-Classes-and-Enums/time.cpp
+++ labs/lab10-Classes-and-Enums/time.cpp
@@ -2,15 +2,16 @@
 Author: Alisherjon Turakulov
 Course: CS 135
 Instructor: Tong Yi
-Assignment: Lab 10C
+Assignment: Lab 10D
 
-building on 10AB
-implements functions interacting with Movie and Enum objects
+building on 10ABC
+implements functions interacting with Movie and Enum objects and timeslots
 
 */
 
 #include <iostream>
 #include <string>
+using namespace std;
 //class and enum definitions
 class Time { 
 public:
@@ -19,7 +20,7 @@ public:
 };
 
 void printTime(Time time) {
-    cout << time.h << ":" << time.m;
+    std::cout << time.h << ":" << time.m;
 }
 
 enum Genre {ACTION, COMEDY, DRAMA, ROMANCE, THRILLER};
@@ -66,24 +67,59 @@ void printMovie(Movie mv){
 Time addMinutes(Time time0, int min){
     int m = min % 60;
     int h = min / 60;
-    
+    int extra = 0;
+    if(time0.m+ m > 60 ){
+        extra = time0.m+m -60;
+        h++;
+    }
    
-    Time added = {(time0.h + h), (time0.m + m) };
+    Time added = {(time0.h + h), (time0.m + extra) };
     return added;
 }
 //output format: "title Genre (time) [starts at s, ends by s+runtime]"
 // "Back to the Future COMEDY (116 min) [starts at 9:15, ends by 11:11]"
 //@param ts, a timeslot object
-printTimeSlot(TimeSlot ts){
+void printTimeSlot(TimeSlot ts){
+    printMovie(ts.movie);
+    std::cout << " [starts at ";
+    printTime(ts.startTime);
+    std::cout << ", ends by ";
+    printTime(addMinutes(ts.startTime, ts.movie.duration));
+    std::cout << "]\n";
+}
 
+//return a new timeslot for nextMovie scheduled at the end time of
+//the timeslot ts
+TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie){
+    Time nextTime = addMinutes(ts.startTime, ts.movie.duration);
+    TimeSlot next = {nextMovie, nextTime};
+    return next;
+}
+
+bool timeOverlap(TimeSlot ts1, TimeSlot ts2){
+    int interval_start;
+    bool over = false;
+    if(minutesSinceMidnight(ts1.startTime) < minutesSinceMidnight(ts2.startTime)){
+        interval_start = minutesUntil(ts1.startTime, ts2.startTime);
+        
+        if(minutesSinceMidnight(ts2.startTime) < (minutesSinceMidnight(ts1.startTime)+ts1.movie.duration) ){
+            over = true;
+        }
+    }else{
+        interval_start = minutesUntil(ts2.startTime, ts1.startTime);
+        if(minutesSinceMidnight(ts1.startTime) < (minutesSinceMidnight(ts2.startTime)+ts2.movie.duration)  ){
+            over = true;
+        }
+    }
+    return over;
 }
 
 int main(){
     int h{}, m{};
-    std::cout << "Enter first time: ";
+    /*std::cout << "Enter first time: ";
     std::cin >> h >> m;
     Time first{h, m};
-    /*std::cout << "Enter second time: ";
+    std::cout << "Enter second time: ";
     std::cin >> h >> m;
     Time second{h, m};
     //call functions to find necessary minutes
@@ -93,27 +129,38 @@ int main(){
     std::cout << '\n';
     std::cout << "These moments of time are " << X << " and " << Y << " minutes after midnight.\n";
     std::cout << "The interval between them is " << Z << " minutes.\n";
-    */
+    
     std::cout <<"Enter minutes to add: ";
     int m2add{};
     std::cin >> m2add;
 
     std::cout << "new minutes to first: " << minutesSinceMidnight(addMinutes(first, m2add)) << std::endl;
-    
+    */
     //morning
     Movie movie1 = {"Minecraft Movie", COMEDY, 120};
-    TimeSlot morning = {movie1, {8, 30}}
+    TimeSlot morning = {movie1, {8, 30}};
+    printTimeSlot(morning);
     //daytime
-    TimeSlot morning = {movie1, {12, 30}}
+    TimeSlot morning2 = {movie1, {12, 30}};
+    printTimeSlot(morning2);
     //evening
-    TimeSlot morning = {movie1, {18, 30}}
+    TimeSlot morning3 = {movie1, {18, 30}};
+    printTimeSlot(morning3);
 
     //day - spirited away
-    Movie movie2 = {"Spirited Away", DRAMA, 100};
-    TimeSlot morning2 = {movie2, {12, 30}};
+    Movie movie2 = {"Spirited Away", DRAMA, 90};
+    TimeSlot day2 = {movie2, {10, 0}};
+    printTimeSlot(day2);
     //day -
     Movie movie3 = {"AOT: the Last", ACTION, 120};
-    TimeSlot morning2 = {movie2, {12, 30}};
+    TimeSlot day3 = {movie3, {11, 30}};
+    printTimeSlot(day3);
+
+    std::cout <<timeOverlap(day2, day3) << std::endl;
+    day3.startTime.m = 29;
+   std::cout << timeOverlap(day3, day2) << std::endl;
+    
+
 
     return 0;
 }
