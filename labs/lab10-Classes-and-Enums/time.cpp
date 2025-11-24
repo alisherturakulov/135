9eaefb03d86c7e16a4d5c00c6ace952ff29f0a23
alisherturakulov <aturakulov100@gmail.comls>
2025-11-23 23:23:36 -0500

    write tests for print timeslots

--- labs/lab10-Classes-and-Enums/time.cpp
+++ labs/lab10-Classes-and-Enums/time.cpp
@@ -2,20 +2,39 @@
 Author: Alisherjon Turakulov
 Course: CS 135
 Instructor: Tong Yi
-Assignment: Lab 10B
+Assignment: Lab 10C
 
-building on 10A
-implements functions to add minutes to a time object
+building on 10AB
+implements functions interacting with Movie and Enum objects
 
 */
 
 #include <iostream>
 #include <string>
-
+//class and enum definitions
 class Time { 
 public:
     int h;
     int m;
+};
+
+void printTime(Time time) {
+    cout << time.h << ":" << time.m;
+}
+
+enum Genre {ACTION, COMEDY, DRAMA, ROMANCE, THRILLER};
+
+class Movie { 
+public: 
+    string title;
+    Genre genre;     // only one genre per movie
+    int duration;    // in minutes
+};
+
+class TimeSlot { 
+public: 
+    Movie movie;     // what movie
+    Time startTime;  // when it starts
 };
 
 //@param time, a Time object
@@ -29,7 +48,21 @@ int minutesUntil(Time earlier, Time later){
     return minutes;
 }
 
+//@param mv, Movie object
+void printMovie(Movie mv){
+    string g;
+    switch (mv.genre) {
+        case ACTION   : g = "ACTION"; break;
+        case COMEDY   : g = "COMEDY"; break;
+        case DRAMA    : g = "DRAMA";  break;
+        case ROMANCE  : g = "ROMANCE"; break;
+        case THRILLER : g = "THRILLER"; break;
+    }
+    cout << mv.title << " " << g << " (" << mv.duration << " min)";
+}
 
+//@param time0, time to add to
+//@param min, integer minutes
 Time addMinutes(Time time0, int min){
     int m = min % 60;
     int h = min / 60;
@@ -37,6 +70,12 @@ Time addMinutes(Time time0, int min){
    
     Time added = {(time0.h + h), (time0.m + m) };
     return added;
+}
+//output format: "title Genre (time) [starts at s, ends by s+runtime]"
+// "Back to the Future COMEDY (116 min) [starts at 9:15, ends by 11:11]"
+//@param ts, a timeslot object
+printTimeSlot(TimeSlot ts){
+
 }
 
 int main(){
@@ -60,5 +99,21 @@ int main(){
     std::cin >> m2add;
 
     std::cout << "new minutes to first: " << minutesSinceMidnight(addMinutes(first, m2add)) << std::endl;
+    
+    //morning
+    Movie movie1 = {"Minecraft Movie", COMEDY, 120};
+    TimeSlot morning = {movie1, {8, 30}}
+    //daytime
+    TimeSlot morning = {movie1, {12, 30}}
+    //evening
+    TimeSlot morning = {movie1, {18, 30}}
+
+    //day - spirited away
+    Movie movie2 = {"Spirited Away", DRAMA, 100};
+    TimeSlot morning2 = {movie2, {12, 30}};
+    //day -
+    Movie movie3 = {"AOT: the Last", ACTION, 120};
+    TimeSlot morning2 = {movie2, {12, 30}};
+
     return 0;
 }
